f75ff25c3481efb713446b7271a719a277d8f788
NicKoehler <grillinicola@proton.me>
2024-10-25 15:39:41 +0200

    add file_premium missing case

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ async fn main() -> Result<()> {
                 return Err(anyhow!("Invalid Mediafire folder URL"));
             }
         }
-        "file" => {
+        "file" | "file_premium" => {
             create_directory_if_not_exists(&path).await?;
             let response = file::get_info(&key).await?;
             if let Some(file_info) = response.file_info {
