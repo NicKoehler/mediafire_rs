574049cc9f5efb030db5190595edb48a9f4dfc1a
NicKoehler <grillinicola@proton.me>
2025-07-17 22:13:07 +0200

    Fix for new mediafire download links

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use base64::prelude::*;
 use regex::Regex;
 use ring::digest;
 use scraper::{Html, Selector};
@@ -27,13 +28,18 @@ pub async fn create_directory_if_not_exists(path: &PathBuf) -> Result<()> {
 pub fn parse_download_link(html: &str) -> Option<String> {
     let document = Html::parse_document(html);
     let selector = Selector::parse("#downloadButton").ok()?;
-    let link = document
+    let base64link = document
         .select(&selector)
         .next()?
         .value()
-        .attr("href")?
+        .attr("data-scrambled-url")?
         .to_string();
-    Some(link)
+    if let Ok(bytes) = BASE64_STANDARD.decode(base64link.as_bytes()) {
+        if let Ok(real_url) = String::from_utf8(bytes) {
+            return Some(real_url);
+        };
+    }
+    None
 }
 
 pub fn check_hash(file_path: &PathBuf, expected_hash: &String) -> Result<bool, std::io::Error> {
@@ -56,7 +62,8 @@ mod tests {
     #[tokio::test]
     async fn test_parse_download_link() {
         let client = Client::new(None, false);
-        let html = client.api_client
+        let html = client
+            .api_client
             .get("https://www.mediafire.com/file/tb1d35twcp7oj3p")
             .send()
             .await
