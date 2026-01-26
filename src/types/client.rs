b24608714b4e93dbb7f2352c72542d5fa61eed93
N4CH723HR3R <n4ch7@n3831.net>
2026-01-26 16:33:32 +0100

    update dependencies and debloat reqwest

--- src/types/client.rs
+++ src/types/client.rs
@@ -10,13 +10,11 @@ pub struct Client {
 impl Client {
     pub fn new(proxies: Option<Vec<String>>, proxy_downloads: bool) -> Self {
         let mut api_builder = reqwest::Client::builder()
-            .use_rustls_tls()
             .default_headers(HeaderMap::from_iter([
                     (USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")),
                     (ACCEPT_ENCODING, HeaderValue::from_static("gzip")),
             ]));
         let mut download_builder = reqwest::Client::builder()
-            .use_rustls_tls()
             .default_headers(HeaderMap::from_iter([
                     (USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")),
                     (ACCEPT_ENCODING, HeaderValue::from_static("gzip")),
@@ -31,7 +29,7 @@ impl Client {
 
         Self {
             api_client: api_builder.build().unwrap(),
-            download_client: download_builder.build().unwrap()
+            download_client: download_builder.build().unwrap(),
         }
     }
 }
